065b0829bf093745f182c952b6014fb210cadada
Gabriel Lacerda <127674463+lacerdagb@users.noreply.github.com>
2024-10-02 08:43:13 -0300

    First Commit
    upload de todos os arquivos do projeto VisualizadorNutricional

---
+++ VisualizadorNutricional/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿using System;
+
+namespace VisualizadorNutricional
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Categoria:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Alimento:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Frutas",
+            "Legumes",
+            "Industrializados",
+            "Bebidas"});
+            this.comboBox1.Location = new System.Drawing.Point(163, 266);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(163, 314);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(158, 21);
+            this.comboBox2.TabIndex = 3;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(360, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Preço Médio:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(43, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(256, 161);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(561, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tabela Nutricional";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(387, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Produto";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(344, 181);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 158);
+            this.panel1.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(547, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(216, 295);
+            this.panel2.TabIndex = 11;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(216, 292);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(801, 366);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ VisualizadorNutricional/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualizadorNutricional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e) // Define tamanho fixo para o pictureBox2 ao ser clicado.
+        {
+            pictureBox2.Width = 200; // Defina a largura
+            pictureBox2.Height = 200; // Defina a altura igual à largura
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Obtém uma lista de arquivos de imagem da pasta de produtos.
+            string[] nomeFotoProdutos = Directory.GetFiles(@"C:\Users\Gabriel\OneDrive\Documentos\Estudos\Projetos\VisualizadorNutricional\imagens");
+            foreach (string img in nomeFotoProdutos) // Para cada imagem encontrada na pasta de produtos.
+            {
+                string[] separador = img.Split('\\'); // Separa o caminho do arquivo em partes, usando o caractere "\".
+                string[] nomeSelecionado = separador[9].Split('.'); // Pega o nome do arquivo sem a extensão.
+
+                if (comboBox2.Text == nomeSelecionado[0]) // Verifica se o item selecionado no comboBox2 corresponde ao nome da imagem.
+                {
+                    // Define a imagem do produto no pictureBox1.
+                    pictureBox1.ImageLocation = @"C:\Users\Gabriel\OneDrive\Documentos\Estudos\Projetos\VisualizadorNutricional\imagens\" + comboBox2.Text + ".png";
+                }
+                // Obtém uma lista de arquivos de imagem da pasta de tabelas nutricionais.
+                string[] nomeFotoTabela = Directory.GetFiles(@"C:\Users\Gabriel\OneDrive\Documentos\Estudos\Projetos\VisualizadorNutricional\tabela");
+                foreach (string img2 in nomeFotoTabela) // Para cada imagem de tabela nutricional encontrada.
+                {
+                    string[] separador2 = img2.Split('\\'); // Separa o caminho do arquivo em partes.
+                    string[] nomeSelecionado2 = separador2[9].Split('.'); // Pega o nome do arquivo sem a extensão.
+
+
+                    if (comboBox2.Text == nomeSelecionado2[0]) // Verifica se o item selecionado no comboBox2 corresponde ao nome da tabela.
+                    {
+                        // Define a tabela nutricional no pictureBox2.
+                        pictureBox2.ImageLocation = @"C:\Users\Gabriel\OneDrive\Documentos\Estudos\Projetos\VisualizadorNutricional\tabela\" + comboBox2.Text + ".png";
+                    }
+                }
+
+                if (comboBox2.Text == nomeSelecionado[0])
+                {
+                    pictureBox2.ImageLocation = @"C:\Users\Gabriel\OneDrive\Documentos\Estudos\Projetos\VisualizadorNutricional\tabela\" + comboBox2.Text + ".png";
+                }
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Text = string.Empty; // Limpa a seleção do comboBox2.
+
+            if (comboBox1.Text == "Frutas") // Define os itens do comboBox2 de acordo com a categoria selecionada.
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Maça");
+                comboBox2.Items.Add("Banana");
+                comboBox2.Items.Add("Abacate");
+            }
+            else if (comboBox1.Text == "Legumes")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Abóbora");
+                comboBox2.Items.Add("Batata");
+                comboBox2.Items.Add("Beterraba");
+            }
+            else if (comboBox1.Text == "Industrializados")
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Biscoito");
+                comboBox2.Items.Add("Macarrão");
+                comboBox2.Items.Add("Creme de leite");
+            }
+            else
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Refrigerante");
+                comboBox2.Items.Add("Iogurte");
+                comboBox2.Items.Add("Energético");
+            }
+
+
+
+        }
+    }
+
+}
